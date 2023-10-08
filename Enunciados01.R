f06e76cd2d0977d082b659a207a9d4c3acc88fd5
PatriciaGuerrero01 <patriguerrero01@gmail.com>
2023-10-08 21:07:03 +0200

    problema 1

---
+++ Enunciados01.R
@@ -0,0 +1,14 @@
+#Enunciados de los problemas
+
+#Problema 1: Sea la tabla de decisión con 5 estados de la naturaleza y 4
+#alternativas de la siguiente forma:
+
+X = matrix(c(5,4,6,2,3,1,7,8,7,5,2,0,8,9,5,4,3,-1,9,10),nrow=4,ncol=3,byrow=TRUE)
+colnames(X)=c('e1','e2','e3','e4','e5')
+rownames(X)=c('d1','d2','d3','d4')
+X
+
+
+
+#Problema 2: Una farmaceutica quiere planificar la producción de un determinado
+#fármaco
