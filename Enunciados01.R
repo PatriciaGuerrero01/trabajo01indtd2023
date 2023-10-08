4f297b5c49b5a2420ae1feb21cf367a0831f312d
PatriciaGuerrero01 <patriguerrero01@gmail.com>
2023-10-08 23:29:29 +0200

    enunciados finalizados

--- Enunciados01.R
+++ Enunciados01.R
@@ -1,14 +1,25 @@
 #Enunciados de los problemas
 
-#Problema 1: Sea la tabla de decisión con 5 estados de la naturaleza y 4
+#PROBLEMA 1: Sea la tabla de decisión con 5 estados de la naturaleza y 4
 #alternativas de la siguiente forma:
 
-X = matrix(c(5,4,6,2,3,1,7,8,7,5,2,0,8,9,5,4,3,-1,9,10),nrow=4,ncol=3,byrow=TRUE)
+X = matrix(c(5,4,6,2,3,1,7,8,7,5,2,0,8,9,5,4,3,-1,9,10),nrow=4,ncol=5,byrow=TRUE)
 colnames(X)=c('e1','e2','e3','e4','e5')
 rownames(X)=c('d1','d2','d3','d4')
 X
 
+#Resolverla tanto para situación favorable como desfavorable, con cada uno de los 
+#criterios por separado.
 
 
-#Problema 2: Una farmaceutica quiere planificar la producción de un determinado
-#fármaco
+#PROBLEMA 2: Un agricultor quiere estudiar el crecimiento de una determinada 
+#cosecha de verdura. Posee cuatro posibles terrenos donde sembrar: Terreno 1 (a1), 
+#Terreno 2 (a2),Terreno 3 (a3), Terreno 4 (a4) ; habrá 4 posibles estados del 
+#clima que influirá en el crecimiento de la cosecha: soleado(d1),nublado(d2),
+#lluvia(d3),lluvia abundante(d4). Y en consecuencia se recolectarán más o menos kilos de
+#verdura que se recogen en la siguiente tabla: 
+
+X = matrix(c(300,540,615,210,345,105,709,298,365,640,100,540,860,192,570,540),nrow=4,ncol=4,byrow=TRUE)
+colnames(X)=c('e1','e2','e3','e4')
+rownames(X)=c('d1','d2','d3','d4')
+X
